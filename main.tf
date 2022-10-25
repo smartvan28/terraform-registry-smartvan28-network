581698cab8ac0f2e3945dda6f74406f3d94bbb59
smartvan28 <65109750+smartvan28@users.noreply.github.com>
2022-10-26 00:10:48 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+
+
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = var.key1
+  cloud_id  = var.cloud_id1
+  folder_id = var.folder_id1
+  zone      = "ru-central1-a"
+}
+
 resource "yandex_vpc_network" "foo" {}
 
 resource "yandex_vpc_subnet" "foo" {
